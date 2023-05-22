6a468be14944c1e2a942fdd27a428c371c8731e6
Alex Warrington <awarrington0895@gmail.com>
2023-05-22 22:47:10 +0000

    Refactor to use a struct for organization

--- day2/d2p1.c
+++ day2/d2p1.c
@@ -2,51 +2,79 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[])
-{
+typedef struct {
+  int depth;
+  int position;
+} SubmarineLocation;
+
+SubmarineLocation initLocation() {
+  SubmarineLocation location = { .depth = 0, .position = 0 };
+
+  return location;
+}
+
+SubmarineLocation moveLocation(
+  SubmarineLocation location,
+  const char* adjustment,
+  int adjustmentSize
+) {
+  // Don't want to mutate the input
+  char copy[adjustmentSize];
+  strcpy(copy, adjustment);
+
+  // Expected format: DIRECTION<Space>MAGNITUDE
+  char* direction = strtok(copy, " ");
+  char* magnitudeToken = strtok(NULL, " ");
+  int magnitude = atoi(magnitudeToken);
+
+  if (strcmp(direction, "forward") == 0) {
+    location.position += magnitude;
+
+    return location;
+  }
+
+  if (strcmp(direction, "down") == 0) {
+    location.depth += magnitude;
+
+    return location;
+  }
+
+  if (strcmp(direction, "up") == 0) {
+    location.depth -= magnitude;
+
+    return location;
+  }
+
+  printf("WARNING: Encountered unmatched direction: %s", direction);
+
+  return location;
+}
+
+void printLocation(const SubmarineLocation* location) {
+  printf("Current position = %d\n", location->position);
+  printf("Current depth = %d\n\n", location->depth);
+
+  printf("Multiplied = %d\n", location->depth * location->position);
+}
+
+int main(int argc, char *argv[]) {
   (void)argc;
   const int bufferSize = 12;
   char buffer[bufferSize];
-  char copy[bufferSize];
   FILE *file = fopen(argv[1], "r");
 
-  if (file == NULL)
-  {
+  if (file == NULL) {
     printf("Unable to open file %s", argv[1]);
     return EXIT_FAILURE;
   }
 
-  int currentDepth = 0;
-  int currentPosition = 0;
+  SubmarineLocation location = initLocation();
 
-  while (fgets(buffer, bufferSize, file) != NULL)
-  {
-    strcpy(copy, buffer);
-
-    // Expected format: DIRECTION<Space>MAGNITUDE
-    char* direction = strtok(copy, " ");
-    char* magnitude = strtok(NULL, " ");
-
-    if (strcmp(direction, "forward") == 0) {
-      currentPosition += atoi(magnitude);
-      continue;
-    }
-
-    if (strcmp(direction, "down") == 0) {
-      currentDepth += atoi(magnitude);
-      continue;
-    }
-
-    if (strcmp(direction, "up") == 0) {
-      currentDepth -= atoi(magnitude);
-      continue;
-    }
-
-    printf("WARNING: Encountered unmatched direction: %s", direction);
+  while (fgets(buffer, bufferSize, file) != NULL) {
+    location = moveLocation(location, buffer, bufferSize); 
   }
 
-  printf("Current position = %d\n", currentPosition);
-  printf("Current depth = %d\n\n", currentDepth);
+  fclose(file);
 
-  printf("Multiplied = %d\n", currentDepth * currentPosition);
+  printLocation(&location);
 }
