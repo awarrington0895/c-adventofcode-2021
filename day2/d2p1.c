ab8c2d62d14d70eb4473495c440e13ff4b281619
Alex <awarrington0895@gmail.com>
2023-05-22 12:23:48 +0000

    Initial printing

--- day2/d2p1.c
+++ day2/d2p1.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-  printf("Day 2 is working!\n");
+int main(int argc, char *argv[])
+{
+  const int bufferSize = 12;
+  char buffer[bufferSize];
+  FILE *file = fopen(argv[1], "r");
+
+  if (file == NULL)
+  {
+    printf("Unable to open file %s", argv[1]);
+    return EXIT_FAILURE;
+  }
+
+  int currentDepth = 0;
+  int currentPosition = 0;
+
+  while (fgets(buffer, bufferSize, file) != NULL)
+  {
+    printf("%s", buffer);
+  }
+
+  printf("\n");
 }
